229011523f95e17f6db77d333b09380747af876c
GorlikItsMe <33355238+GorlikItsMe@users.noreply.github.com>
2025-02-05 19:13:24 +0100

    make loop work longer

--- loop.py
+++ loop.py
@@ -10,35 +10,40 @@ connection_string = f'http://{Config.username}:{Config.password}@{Config.router_
 
 if __name__ == "__main__":
     while True:
-        with Connection(connection_string) as connection:
-            client = Client(connection)
-            is4g = client.monitoring.status()["SignalIconNr"] == "0"
-            print(f"4G: {is4g}")
-            if is4g:
-                print("its 4G. Lets try fix that!")
-                send_discord_notification(
-                    "5G Connection Issue Detected: Attempting to fix...")
+        try:
+            with Connection(connection_string) as connection:
+                client = Client(connection)
+                is4g = client.monitoring.status()["SignalIconNr"] == "0"
+                print(f"4G: {is4g}")
+                if is4g:
+                    print("its 4G. Lets try fix that!")
+                    send_discord_notification(
+                        "5G Connection Issue Detected: Attempting to fix...")
 
-                data = client.net.net_mode()
-                print(data)
-                print("switch to 4G only")
-                client.net.set_net_mode(
-                    lteband=data['LTEBand'],
-                    networkband=data['NetworkBand'],
-                    networkmode=NetworkModeEnum.MODE_4G_ONLY
-                )
-                print("wait 10sek for network to switch...")
-                sleep(10)
-                data = client.net.net_mode()
-                print(data)
-                print("set back to auto (5G)")
-                client.net.set_net_mode(
-                    lteband=data['LTEBand'],
-                    networkband=data['NetworkBand'],
-                    networkmode=NetworkModeEnum.MODE_AUTO
-                )
-                send_discord_notification("Done. Now it should work.")
-            else:
-                print("You are already on 5G")
-            print("wait 1min...")
-            sleep(60)
+                    data = client.net.net_mode()
+                    print(data)
+                    print("switch to 4G only")
+                    client.net.set_net_mode(
+                        lteband=data['LTEBand'],
+                        networkband=data['NetworkBand'],
+                        networkmode=NetworkModeEnum.MODE_4G_ONLY
+                    )
+                    print("wait 10sek for network to switch...")
+                    sleep(10)
+                    data = client.net.net_mode()
+                    print(data)
+                    print("set back to auto (5G)")
+                    client.net.set_net_mode(
+                        lteband=data['LTEBand'],
+                        networkband=data['NetworkBand'],
+                        networkmode=NetworkModeEnum.MODE_AUTO
+                    )
+                    send_discord_notification("Done. Now it should work.")
+                else:
+                    print("You are already on 5G")
+                print("wait 1min...")
+                sleep(60)
+        except Exception as e:
+            print(e)
+            print("Exception was throwed. Im waiting 10sek and restarting that thing")
+            sleep(10)
